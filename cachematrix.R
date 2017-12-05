426cddd6847d3a8c18f6973a24d083ac9c14609a
Tocheee <tochi.okeke@gmail.com>
2017-12-04 18:42:04 -0800

    Adding my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Tocheee
+##Programming Assignment 2
+## These functions work together to cache the inverse of a matirx in order to save computation time.
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
+## cacheSolve first checks to see if an inverse has already been calculated and returns that inverse, otherwise it computes a new inverse with the new matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates a matrix object that is able to cache its inverse.
 
+makeCacheMatrix <- function (x = matrix()){
+  m <- NULL
+  set <- function(y){
+    x <<-y       ## assigns the argument y to the object x. To set new data without re-running the entire code.
+    m <<- NULL   ## clears the value of m that was previously cached 
+  }
+  get <-function() x    ## returns matrix 'x'
+  setinverse <- function(solve) m <<- solve  ## assigns the inverse to be calculated in the second function to 'm'
+  getinverse <- function() m  ## returns the inverse 'm'
+  list (set = set, get = get, setinverse = setinverse, getinverse = getinverse) ## returns a list of functions to be used for retrieving data in cacheSolve
 }
 
+## cacheSolve first checks for a saved inverse. If false, it calculates the inverse of the new matrix and saves it in 'm'
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x,...){
+  
+  m <- x$getinverse() ## retrieving the matrix 'm'
+  if (!is.null(m)) {
+    message ("getting cached data") ## checks for a saved inverse and returns message if TRUE
+  }
+  data <- x$get() ## If FALSE, retrieves data from get()
+  m <- solve(data) ## Calculates the inverse of the matrix in data and stores it in 'm'
+  x$setinverse(m) ## Sets 'm' to the inverse matrix
+  m ##print m
+} 
